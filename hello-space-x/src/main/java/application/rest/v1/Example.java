b2367f9c3c8c57f30f8dd84792875d7902949331
david young <root@icp-network-12.rtp.raleigh.ibm.com>
2018-02-07 22:29:09 -0500

    one more

--- hello-space-x/src/main/java/application/rest/v1/Example.java
+++ hello-space-x/src/main/java/application/rest/v1/Example.java
@@ -21,6 +21,7 @@ public class Example {
         List<String> list = new ArrayList<>();
         //return a simple list of strings
         list.add("Congratulations, your application is up and running");
+        list.add("One More");
         return Response.ok(list.toString()).build();
     }
 
